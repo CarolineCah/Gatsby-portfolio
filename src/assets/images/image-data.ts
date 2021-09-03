9300f34becc290f28d771a60a2e1e4c438a16d29
Carolinecah <caroline.cah@polestar.com>
2021-09-03 20:51:29 +0200

    remove picture

--- src/assets/images/image-data.ts
+++ src/assets/images/image-data.ts
@@ -1,5 +1,4 @@
 export const images = [
   'https://portfoliopictures.s3.eu-north-1.amazonaws.com/carolinecah1',
   'https://portfoliopictures.s3.eu-north-1.amazonaws.com/carolinecah2.png',
-  'https://portfoliopictures.s3.eu-north-1.amazonaws.com/carolinecah1',
 ];
